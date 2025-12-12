aceb6feb0add8206f446217d381b3fb0d27ae1b8
DainamonDev <Andradescesar2024@gmail.com>
2025-12-12 19:50:47 -0400

    fix: corregir bug de navegación a niveles - implementar carga async

--- src/RandomRouletteLayer.cpp
+++ src/RandomRouletteLayer.cpp
@@ -13,72 +13,87 @@ RandomRouletteLayer* RandomRouletteLayer::create() {
 
 bool RandomRouletteLayer::setup() {
     this->setTitle("Random Demon Roulette");
-
+    
     RandomRouletteManager::getInstance()->init();
-
+    
     // UI Elements
     m_nameLabel = CCLabelBMFont::create("...", "bigFont.fnt");
     m_nameLabel->setScale(0.6f);
     m_nameLabel->setPosition(m_size.width / 2, m_size.height / 2 + 25);
     m_mainLayer->addChild(m_nameLabel);
-
+    
     m_diffLabel = CCLabelBMFont::create("Difficulty", "chatFont.fnt");
     m_diffLabel->setPosition(m_size.width / 2, m_size.height / 2);
     m_mainLayer->addChild(m_diffLabel);
-
+    
     m_progressLabel = CCLabelBMFont::create("Progress: 0%", "chatFont.fnt");
     m_progressLabel->setPosition(m_size.width / 2, m_size.height / 2 - 20);
     m_mainLayer->addChild(m_progressLabel);
-
+    
     // Buttons
     auto menu = CCMenu::create();
     menu->setPosition(0, 0);
     m_mainLayer->addChild(menu);
-
+    
     auto playSpr = ButtonSprite::create("Play", "goldFont.fnt", "GJ_button_01.png", .8f);
     auto playBtn = CCMenuItemSpriteExtra::create(playSpr, this, menu_selector(RandomRouletteLayer::onPlay));
     playBtn->setPosition(m_size.width / 2 - 80, 40);
     menu->addChild(playBtn);
-
+    
     auto nextSpr = ButtonSprite::create("Next", "goldFont.fnt", "GJ_button_02.png", .8f);
     auto nextBtn = CCMenuItemSpriteExtra::create(nextSpr, this, menu_selector(RandomRouletteLayer::onNext));
     nextBtn->setPosition(m_size.width / 2 + 80, 40);
     menu->addChild(nextBtn);
-
+    
     auto resetSpr = CCSprite::createWithSpriteFrameName("GJ_resetBtn_001.png");
     resetSpr->setScale(0.8f);
     auto resetBtn = CCMenuItemSpriteExtra::create(resetSpr, this, menu_selector(RandomRouletteLayer::onReset));
     resetBtn->setPosition(m_size.width - 25, m_size.height - 25);
     menu->addChild(resetBtn);
-
+    
     updateUI();
+    
     return true;
 }
 
 void RandomRouletteLayer::updateUI() {
     auto mgr = RandomRouletteManager::getInstance();
     auto level = mgr->getCurrentLevel();
-
+    
     if (!level) {
         m_nameLabel->setString("Click Next");
         m_diffLabel->setString("");
         m_progressLabel->setString("");
         return;
     }
-
+    
     m_nameLabel->setString(level->name.c_str());
-
+    
     ccColor3B col = {255, 255, 255};
     std::string txt = "";
+    
     switch(level->difficulty) {
-        case DemonLevel::Difficulty::Easy: txt="Easy Demon"; col={0,255,0}; break;
-        case DemonLevel::Difficulty::Medium: txt="Medium Demon"; col={255,255,0}; break;
-        case DemonLevel::Difficulty::Hard: txt="Hard Demon"; col={255,128,0}; break;
-        case DemonLevel::Difficulty::Extreme: txt="Extreme Demon"; col={255,0,0}; break;
+        case DemonLevel::Difficulty::Easy: 
+            txt = "Easy Demon"; 
+            col = {0, 255, 0}; 
+            break;
+        case DemonLevel::Difficulty::Medium: 
+            txt = "Medium Demon"; 
+            col = {255, 255, 0}; 
+            break;
+        case DemonLevel::Difficulty::Hard: 
+            txt = "Hard Demon"; 
+            col = {255, 128, 0}; 
+            break;
+        case DemonLevel::Difficulty::Extreme: 
+            txt = "Extreme Demon"; 
+            col = {255, 0, 0}; 
+            break;
     }
+    
     m_diffLabel->setString(txt.c_str());
     m_diffLabel->setColor(col);
-
+    
     if (mgr->isCompleted(level->id)) {
         m_progressLabel->setString("Completed!");
         m_progressLabel->setColor({0, 255, 0});
@@ -92,13 +107,37 @@ void RandomRouletteLayer::updateUI() {
 void RandomRouletteLayer::onPlay(CCObject*) {
     auto level = RandomRouletteManager::getInstance()->getCurrentLevel();
     if (!level) return;
+    
+    // Download level from server
+    auto glm = GameLevelManager::sharedState();
+    glm->m_levelManagerDelegate = this;
+    glm->getOnlineLevel(level->id, false);
+    
+    Notification::create("Loading level...", NotificationIcon::Loading)->show();
+    this->retain();  // Keep layer alive during async load
+}
 
-    // Correct API Usage: LevelBrowserLayer with GJSearchObject Type19 (Level ID)
-    auto searchObj = GJSearchObject::create(SearchType::Type19, std::to_string(level->id));
-    auto scene = LevelBrowserLayer::scene(searchObj);
-    CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, scene));
+void RandomRouletteLayer::loadLevelsFinished(cocos2d::CCArray* levels, const char* key) {
+    this->release();
+    
+    if (!levels || levels->count() == 0) {
+        FLAlertLayer::create("Error", "Level not found on server", "OK")->show();
+        return;
+    }
+    
+    auto gjLevel = static_cast<GJGameLevel*>(levels->objectAtIndex(0));
+    
+    // Open the level in PlayLayer
+    auto searchObj = GJSearchObject::create(SearchType::Type19, std::to_string(gjLevel->m_levelID));
+    auto browser = LevelBrowserLayer::scene(searchObj);
+    CCDirector::sharedDirector()->replaceScene(CCTransitionFade::create(0.5f, browser));
     
     this->onClose(nullptr);
+}
+
+void RandomRouletteLayer::loadLevelsFailed(const char* key) {
+    this->release();
+    FLAlertLayer::create("Error", "Failed to download level", "OK")->show();
 }
 
 void RandomRouletteLayer::onNext(CCObject*) {
--- src/RandomRouletteLayer.hpp
+++ src/RandomRouletteLayer.hpp
@@ -3,20 +3,26 @@
 
 using namespace geode::prelude;
 
-class RandomRouletteLayer : public Popup<> {
+class RandomRouletteLayer : public Popup<>, public LevelManagerDelegate {
 protected:
     bool setup() override;
-
+    
     CCLabelBMFont* m_nameLabel;
     CCLabelBMFont* m_diffLabel;
     CCLabelBMFont* m_progressLabel;
-
+    
     void onPlay(CCObject*);
     void onNext(CCObject*);
     void onReset(CCObject*);
     
     void updateUI();
-
+    
+    // LevelManagerDelegate callbacks
+    void loadLevelsFinished(cocos2d::CCArray* levels, const char* key) override;
+    void loadLevelsFailed(const char* key) override;
+    void loadLevelsFinished(cocos2d::CCArray* levels, const char* key, int) override {}
+    void loadLevelsFailed(const char* key, int) override {}
+    
 public:
     static RandomRouletteLayer* create();
 };
