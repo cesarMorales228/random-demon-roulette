f492182d00978f09151982b4bcb03611ac5564b1
DainamonDev <andradescesar2024@gmail.com>
2025-12-12 21:06:52 -0400

    fix: remove RandomRouletteManager references from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <Geode/Geode.hpp>
-#include "RandomRouletteManager.hpp"
-#include <matjson.hpp>
 
 using namespace geode::prelude;
 
 $execute {
-    RandomRouletteManager::getInstance()->init();
-    log::info("Random Demon Roulette Initialized (Verified APIs)");
+    log::info("Level Recommender Initialized (Verified APIs)");
 }
